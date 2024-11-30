6dd29d75ccc24a9e85156f7437ffe52ba753bde3
Shoaib Ansari <shoaib.ansari8600@gmail.com>
2024-11-30 18:31:37 +0530

    added game-total-info/:subAdminId

--- routes/subAdminRoutes.js
+++ routes/subAdminRoutes.js
@@ -1,15 +1,18 @@
 import express from "express";
 import { authAdmin, authSubAdmin } from "../middleware/auth.js";
-import { 
-    create,
-    login,
-    logout
-} from '../controllers/subAdminController.js'
+import {
+  create,
+  getSubAdminGameTotalInfo,
+  login,
+  logout,
+} from "../controllers/subAdminController.js";
 
 const router = express.Router();
 
 router.post("/create", authAdmin, create);
 router.post("/login", login);
-router.post("/logout", authSubAdmin ,logout);
+router.post("/logout", authSubAdmin, logout);
+
+router.get("/game-total-info/:subAdminId", getSubAdminGameTotalInfo);
 
 export default router;
