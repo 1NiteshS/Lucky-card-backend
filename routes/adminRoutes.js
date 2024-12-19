89c00ffd1532c061ffd956f64105ada020577ba1
Nitish <nitishsingh@midknightstudio.com>
2024-12-19 10:52:41 +0530

    first commit

---
+++ routes/adminRoutes.js
@@ -0,0 +1,72 @@
+// routes/adminRoutes.js
+import express from "express";
+import {
+  create,
+  login,
+  verifyOTP,
+  forgotPassword,
+  resetPassword,
+  getAllAdmins,
+  getAdminProfile,
+  getCurrentGame,
+  updatePassword,
+  postAllAdminWinnings,
+  getAdminWinnings,
+  getAdminGameTotalInfo,
+  logout,
+  transferMoney,
+  setCommission,
+  getSubAdminByAdmin,
+  dashLogin,
+  resetSubAdminLogin,
+} from "../controllers/adminController.js";
+import { authAdmin, authSuperAdmin } from "../middleware/auth.js";
+import {
+  claimAllWinnings,
+  getAdminGameResultsForAdmin,
+  getTotalWinnings,
+} from "../controllers/cardController.js";
+import { searchAll } from "../controllers/searchController.js";
+import { resetSubPassword, } from "../controllers/subAdminController.js"
+
+const router = express.Router();
+
+router.post("/create", authSuperAdmin, create);
+router.post("/login", login);
+router.post("/logout",authAdmin, logout);
+router.post("/verify-otp", verifyOTP);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+router.get("/all-admins", getAllAdmins);
+router.get("/profile/:userId/:type", getAdminProfile);
+router.get("/current-game", getCurrentGame);
+router.post("/update-password", updatePassword);
+router.post("/postAllAdminWinnings/:adminId", postAllAdminWinnings);
+router.get("/winnings/:adminId", authAdmin, getAdminWinnings);
+
+router.get("/admin-game-results/:userId/:type",getAdminGameResultsForAdmin);
+
+router.get("/search-result", searchAll);
+
+// Get total winnings for an admin
+router.get("/total-winnings/:adminId", authAdmin, getTotalWinnings);
+
+// Claim all winnings for an admin
+router.post("/claim-all/:adminId", claimAllWinnings);
+
+router.get("/game-total-info/:adminId", getAdminGameTotalInfo);
+
+// New
+router.post("/transfer-money", transferMoney);
+// New
+router.post("/set-commission", setCommission);
+// New
+router.get("/subadmins/:adminId", getSubAdminByAdmin);
+// New
+router.post('/dashLogin', dashLogin)
+// New
+router.get('/subadmin/reset-login/:subAdminId', resetSubAdminLogin);
+// New
+router.post('/subadmin/reset-password/:subAdminId', resetSubPassword);
+
+export default router;
---
+++ routes/cardRoutes.js
@@ -0,0 +1,111 @@
+import express from "express";
+import {
+  calculateAmounts,
+  claimWinnings,
+  getAdminGameResult,
+  // getAdminGameResults,
+  getAdminResults,
+  getAllCards,
+  // getAllRecentWinningCards,
+  getAllSelectedCards,
+  getCurrentGame,
+  getLatestSelectedCards,
+  placeBet,
+  postCardNumber,
+  processAllSelectedCards,
+  placeAutomatedBet,
+  getAdminLatestBets,
+  deleteBetByTicketId,
+} from "../controllers/cardController.js";
+import { authAdmin, authSubAdmin } from "../middleware/auth.js";
+//New
+import  PercentageMode  from "../models/PercentageMode.js"
+
+const router = express.Router();
+
+// Start the timer
+// router.post('/start-timer', startTimer);
+
+// Route to calculate total, lowest, and perform operations
+router.get("/calculate", calculateAmounts);
+
+// Route to place a bet
+router.post("/bet", authAdmin, placeBet);
+
+// New
+// Route for SubAdmin to place a bet
+router.post('/bet/subadmin/:adminId', authSubAdmin, placeBet);
+
+// In your routes file
+router.get('/getBets/:userId/:type', getAdminLatestBets);
+
+// DELETE /api/bets/:ticketId
+router.delete('/deleteBets/:ticketId/:type', deleteBetByTicketId);
+
+router.post("/betBot", placeAutomatedBet);
+
+// Route to get all cards
+router.get("/all-cards", getAllCards);
+
+// Route to post card number
+router.post("/card-number", postCardNumber);
+
+// Route to get current game
+router.get("/current-game", getCurrentGame);
+
+router.get("/selected-cards", getAllSelectedCards);
+
+// router.get('/admin-game-results/:gameId', getAdminGameResults);
+
+router.get("/admin-game", getAdminGameResult);
+
+router.get("/admin-results/:userId/:type", getAdminResults);
+
+router.post("/claim", claimWinnings);
+
+router.post("/save-selected-cards", processAllSelectedCards);
+
+// router.get("/recent-winning-cards", getAllRecentWinningCards);
+
+router.get("/recent-winning-cards", getLatestSelectedCards);
+
+
+// New
+// Get current percentage mode
+router.get('/getpercentage-mode', async (req, res) => {
+  try {
+    let mode = await PercentageMode.findOne();
+    if (!mode) {
+      mode = await PercentageMode.create({
+        mode: 'automatic'
+      });
+    }
+    res.json(mode);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// New
+// Update percentage mode
+router.put('/percentage-mode', async (req, res) => {
+  try {
+    const { mode } = req.body;
+    let percentageMode = await PercentageMode.findOne();
+    
+    if (!percentageMode) {
+      percentageMode = new PercentageMode();
+    }
+    
+    if (mode) percentageMode.mode = mode;
+    percentageMode.updatedAt = new Date();
+    
+    await percentageMode.save();
+    res.json(percentageMode);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+export default router;
